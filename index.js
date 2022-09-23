d6ff6d9d857d43abf2e8bfc8f2ecd23b619c5483
GooshTest2 <gautier.fenaux@protonmail.com>
2022-09-23 20:14:43 +0200

    ajout séjour normandie

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const sectionPrice = document.getElementById('section-price');
 
 
 window.addEventListener("resize", function() {
-  if (window.innerWidth <= 768) {
-  sectionPrice.classList.remove("flex")
-  } else {
+  if (window.innerWidth >= 768) {
     sectionPrice.classList.add("flex")
+  } else {
+    sectionPrice.classList.remove("flex")
   };
 });
 
@@ -44,10 +44,10 @@ function navLinkClick() {
 window.onscroll = function() {myFunction()};
 
 // Get the header
-var header = document.getElementById("navbar");
+let header = document.getElementById("navbar");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
+let sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
@@ -57,3 +57,68 @@ function myFunction() {
     header.classList.remove("sticky");
   }
 }
+
+
+
+let image = document.getElementById("weekend-image");
+let currentPos = 0;
+let images = ["bord_de_plage.jpg", "falaise.jpg", "falaise_2.jpg", "falaise_3.jpg", "plage_people.jpg"]
+
+function changePicture() {
+    if (++currentPos >= images.length)
+        currentPos = 0;
+    image.src = 'assets/images/' + images[currentPos];
+}
+
+
+
+
+function fadeIn()
+{
+    image.classList.remove("fade-out");
+    image.classList.add("fade-in");    
+}
+
+function fadeOut()
+{
+    image.classList.remove("fade-in");
+    image.classList.add("fade-out");
+    // Add listener to the "transitionend" event.
+    
+    image.addEventListener("transitionend", function handleChanges()
+    {
+        // Remove the previously added listener, change
+        // the image and fade-in the new image.
+        
+        image.removeEventListener("transitionend", handleChanges);
+        changePicture();
+        fadeIn();
+    });
+}
+
+setInterval(fadeOut, 4100);
+
+const weekendPresentation = document.getElementById('weekend-presentation');
+const arrowWeekend = document.getElementById('arrow-weekend');
+console.log(arrowWeekend)
+const buttonDesktop = document.getElementById('btn-desktop');
+const buttonMobile = document.getElementById('btn-mobile');
+
+
+buttonDesktop.addEventListener('click', () => {
+  weekendPresentation.style.display = 'block'
+  weekendPresentation.style.animation = 'slideInFromLeft 1s ease-in'
+})
+
+buttonMobile.addEventListener('click', () => {
+  weekendPresentation.style.display = 'block'
+  weekendPresentation.style.animation = 'slideInFromLeft 1s ease-in'
+  weekendPresentation.classList.add('flex')
+
+})
+
+arrowWeekend.addEventListener('click', () => {
+  weekendPresentation.style.animation = 'slideOutToLeft 1s ease-in'
+  weekendPresentation.style.animationFillMode =  'forwards'
+
+})
